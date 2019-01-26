8dd646ff16c23d3caf015150ea30088f8b62d784
Edward Nguyen <eddy-nguyen@hotmail.com>
2019-01-26 18:08:15 -0500

    Added toggle buttons and added submit search button

--- Developer/Frontend/tunelight/src/App.js
+++ Developer/Frontend/tunelight/src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 import { fade } from '@material-ui/core/styles/colorManipulator';
 import { withStyles } from '@material-ui/core/styles';
 import SearchAppBar from './Components/SearchAppBar';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
 
 const styles = theme => ({
   root: {
@@ -65,15 +70,100 @@ const styles = theme => ({
   },
 });
 
+const toggleContainer = {
+  backgroundColor: "#f50057",
+  margin: 0,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: '',
+      filters: "",
+      fromDate: "",
+      toDate: "",
+    };
+    this.handleData = this.handleData.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    this.handleFromDateChange = this.handleFromDateChange.bind(this);
+    this.handleToDateChange = this.handleToDateChange.bind(this);
+  }
+
+  handleData(data) {
+    this.setState({userInput: data});
+  }
+
+  handleToggle(event, filters) {
+    this.setState( {filters: filters} )
+  }
+
+  handleFromDateChange(event) {
+    this.setState({fromDate: event.target.value})
+  }
+
+  handleToDateChange(event) {
+    this.setState({toDate: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            <SearchAppBar classes={styles}></SearchAppBar>
+            <SearchAppBar classes={styles} handlerFromParent={this.handleData}></SearchAppBar>
+            <div>{this.state.userInput}</div>
+            <Grid container spacing={24} style={{"margin-top": "1rem"}} alignItems="center">
+              <Grid item xs={12}>
+                  <ToggleButtonGroup exclusive value={this.state.filters} onChange={this.handleToggle} style={toggleContainer}>
+                    <ToggleButton value="genre" style={{"color": "black"}}>
+                      Genre
+                    </ToggleButton>
+                    <ToggleButton value="artist" style={{"color": "black"}}>
+                      Artist
+                    </ToggleButton>
+                    <ToggleButton value="song" style={{"color": "black"}}>
+                      Song
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+              </Grid>
+              <Grid item xs={6}>
+                <Button variant="contained" color="secondary" disableFocusRipple disableRipple>
+                  <form noValidate >
+                    <TextField
+                        id="date"
+                        label="FROM"
+                        type="date"
+                        defaultValue="2017-05-24"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        onChange={this.handleFromDateChange}
+                    />
+                  </form>
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button variant="contained" color="secondary" disableFocusRipple disableRipple>
+                  <form noValidate >
+                    <TextField
+                        id="date"
+                        label="TO"
+                        type="date"
+                        defaultValue="2017-05-24"
+                        InputLabelProps={{
+                          shrink: true,
+                        }}
+                        onChange={this.handleToDateChange}
+                    />
+                  </form>
+                </Button>
+              </Grid>
+            </Grid>
           </p>
         </header>
       </div>
--- Developer/Frontend/tunelight/src/Components/SearchAppBar.js
+++ Developer/Frontend/tunelight/src/Components/SearchAppBar.js
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
+import Button from '@material-ui/core/Button';
 // import MenuIcon from '@material-ui/icons/Menu';
 // import SearchIcon from '@material-ui/icons/Search';
 
@@ -15,7 +16,12 @@ export default class SearchAppBar extends React.Component{
             classes: props,
             userInput: "",
         };
+        this.handleClick = this.handleClick.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    }
+    handleClick(event) {
+        // This line gives a prop to this component so the parent can use and retrieve data.
+        this.props.handlerFromParent(this.state.userInput);
     }
     handleChange(event) {
         this.setState({userInput: event.target.value});
@@ -30,21 +36,14 @@ export default class SearchAppBar extends React.Component{
                         </IconButton>
                         <div className={this.state.classes.grow} />
                         <div className={this.state.classes.search}>
-                            <div className={this.state.classes.searchIcon}>
-                                {/*<SearchIcon />*/}
-                            </div>
-                            <InputBase
-                                placeholder="Search…"
-                                classes={{
-                                    root: this.state.classes.inputRoot,
-                                    input: this.state.classes.inputInput,
-                                }}
-                                onChange={this.handleChange}
-                            />
+                        <div className={this.state.classes.searchIcon}>
+                            {/*<SearchIcon />*/}
+                        </div>
+                            <InputBase placeholder="Search…" classes={{root: this.state.classes.inputRoot, input: this.state.classes.inputInput,}} onChange={this.handleChange}/>
+                            <Button onClick={this.handleClick}>Submit</Button>
                         </div>
                     </Toolbar>
                 </AppBar>
-                <div>{this.state.userInput}</div>
             </div>
         );
     }
