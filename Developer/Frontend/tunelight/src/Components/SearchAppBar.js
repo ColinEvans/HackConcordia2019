0587f8ca2dbc9fb5fa8ea90b4605c2235b66313e
Edward Nguyen <eddy-nguyen@hotmail.com>
2019-01-26 22:50:05 -0500

    Mostly done the front/beginning form page

--- Developer/Frontend/tunelight/src/Components/SearchAppBar.js
+++ Developer/Frontend/tunelight/src/Components/SearchAppBar.js
@@ -8,6 +8,7 @@ import InputBase from '@material-ui/core/InputBase';
 import Button from '@material-ui/core/Button';
 // import MenuIcon from '@material-ui/icons/Menu';
 // import SearchIcon from '@material-ui/icons/Search';
+import TextField from '@material-ui/core/TextField';
 
 export default class SearchAppBar extends React.Component{
     constructor(props) {
@@ -21,29 +22,18 @@ export default class SearchAppBar extends React.Component{
     }
     handleClick(event) {
         // This line gives a prop to this component so the parent can use and retrieve data.
-        this.props.handlerFromParent(this.state.userInput);
+        // this.props.handlerFromParent(this.state.userInput);
     }
     handleChange(event) {
-        this.setState({userInput: event.target.value});
+        var input = event.target.value;
+        this.setState({userInput: input});
+        this.props.handlerFromParent(input);
     }
     render(){
+        const searchLabel = this.props.searchLabel;
         return (
-            <div className={this.state.classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton className={this.state.classes.menuButton} color="inherit" aria-label="Open drawer">
-                            {/*<MenuIcon />*/}
-                        </IconButton>
-                        <div className={this.state.classes.grow} />
-                        <div className={this.state.classes.search}>
-                        <div className={this.state.classes.searchIcon}>
-                            {/*<SearchIcon />*/}
-                        </div>
-                            <InputBase placeholder="Search…" classes={{root: this.state.classes.inputRoot, input: this.state.classes.inputInput,}} onChange={this.handleChange}/>
-                            <Button onClick={this.handleClick}>Submit</Button>
-                        </div>
-                    </Toolbar>
-                </AppBar>
+            <div className={this.state.classes.root} style={{"padding": "1rem"}}>
+                <TextField label={searchLabel} style={{"width": "100%"}} classes={{root: this.state.classes.inputRoot, input: this.state.classes.inputInput,}} onChange={this.handleChange}/>
             </div>
         );
     }
